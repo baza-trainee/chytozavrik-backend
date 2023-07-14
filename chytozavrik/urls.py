eb69d8d6e3032583ec128f87aeff609293cfc760
Danil Tumanov <dania.tumanov@gmail.com>
2023-07-14 14:24:08 +0300

    fix url problem

--- chytozavrik/urls.py
+++ chytozavrik/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('urls.user_profile'))
+    path('api/v1/', include('user_profile.urls'))
 ]
