c44063fc96822f4750aaddfac96219dcd4407bde
Danil Tumanov <dania.tumanov@gmail.com>
2023-07-20 15:02:35 +0300

    create UserViewSet

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .serializers import UserSerializer
+from rest_framework import viewsets
+from .models import User
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
