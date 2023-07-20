cffbd208841d031b428de2ab9e3e9d7c489aeda4
Danil Tumanov <dania.tumanov@gmail.com>
2023-07-20 15:17:42 +0300

    create routs for user viewset

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,3 +1,9 @@
+from rest_framework.routers import DefaultRouter
+from . import views
+
 app_name = 'user_profile'
 
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet, basename='user')
 urlpatterns = []
+urlpatterns += router.urls
