7b55edd69f4646de13b62916d00097e0a437b067
Danil Tumanov <dania.tumanov@gmail.com>
2023-07-21 13:25:03 +0300

    add validator for UserSerializer

--- user_profile/serializers.py
+++ user_profile/serializers.py
@@ -3,10 +3,28 @@ from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+
+    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+
     class Meta:
         model = User
         exclude = ['groups', "user_permissions"]
         extra_kwargs = {'password': {'write_only': True}}
+
+    def validate(self, data):
+        password = data.get('password')
+        password2 = data.pop('password2')
+
+        if password != password2:
+            raise serializers.ValidationError({"password": "Passwords do not match."})
+
+        if len(password) < 6:
+            raise serializers.ValidationError({"password": "Password must be at least 6 characters long."})
+
+        if not any(char.isdigit() for char in password):
+            raise serializers.ValidationError({"password": "Password must contain at least one digit."})
+
+        return data
 
     def create(self, validated_data):
         user = User.objects.create_user(
