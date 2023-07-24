52c9d4b562cb0f4454c1b81cb667867dc882cea8
Danil Tumanov <dania.tumanov@gmail.com>
2023-07-24 21:03:19 +0300

    add MePIView

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,9 +1,12 @@
 from rest_framework.routers import DefaultRouter
+from django.urls import path
 from . import views
 
 app_name = 'user_profile'
 
 router = DefaultRouter()
 router.register(r'users', views.UserViewSet, basename='user')
-urlpatterns = []
+urlpatterns = [
+    path('users/me/', views.MeAPIView.as_view(), name='me'),
+]
 urlpatterns += router.urls
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,8 +1,22 @@
 from .serializers import UserSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, generics, permissions
 from .models import User
 
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+class MeAPIView(generics.RetrieveAPIView):
+    """
+    A view that allows the logged in user to see their details.
+    """
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        """
+        Return the logged in user
+        """
+        return self.request.user
