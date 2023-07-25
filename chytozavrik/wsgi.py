afddb325a7d79f77b8a5b08d7cb9e01f0e0500ab
Danil Tumanov <dania.tumanov@gmail.com>
2023-07-25 21:05:51 +0300

    set up wsgi

--- chytozavrik/wsgi.py
+++ chytozavrik/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chytozavrik.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chytozavrik.settings.prod')
 
 application = get_wsgi_application()
