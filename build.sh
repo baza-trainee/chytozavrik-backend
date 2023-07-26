e7cf0632fff3e9fbe2f329698feec185a2b339fd
Danil Tumanov <dania.tumanov@gmail.com>
2023-07-26 13:23:21 +0300

    add the creation of virtual environment

--- build.sh
+++ build.sh
@@ -2,7 +2,16 @@
 # exit on error
 set -o errexit
 
+# create a virtual environment named 'venv'
+python3 -m venv venv
+
+# activate the virtual environment.
+source venv/bin/activate
+
+# upgrade pip in the virtual environment
 pip install --upgrade pip
+
+# install requirements in the virtual environment
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
