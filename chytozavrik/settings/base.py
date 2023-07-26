2e478c5eb8304be8f160bac59ccd6bf7cda805f2
Danil Tumanov <dania.tumanov@gmail.com>
2023-07-26 13:39:48 +0300

    update base dir location

--- chytozavrik/settings/base.py
+++ chytozavrik/settings/base.py
@@ -15,7 +15,7 @@ from dotenv import load_dotenv
 from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # loads the configs from .env
 load_dotenv()
