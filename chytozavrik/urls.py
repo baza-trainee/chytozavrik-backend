26cededa64fb6270123d184955f3df5c00977d24
Danil Tumanov <dania.tumanov@gmail.com>
2023-07-26 12:47:34 +0300

    set up staticfiles

--- chytozavrik/urls.py
+++ chytozavrik/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from .yasg import urlpatterns as doc_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,7 @@ urlpatterns = [
 ]
 
 urlpatterns += doc_urls
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
