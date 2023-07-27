b7a8554c0f56b2c483214232802170575c3e79bb
Danil Tumanov <dania.tumanov@gmail.com>
2023-07-27 14:59:20 +0300

    create user serializers for swagger

--- user_profile/serializers.py
+++ user_profile/serializers.py
@@ -36,3 +36,23 @@ class UserSerializer(serializers.ModelSerializer):
         )
 
         return user
+
+
+class UserSwaggerPostSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(min_length=6, style={'input_type': 'password'}, write_only=True)
+    password = serializers.CharField(min_length=6, style={'input_type': 'password'}, write_only=True)
+    email = serializers.EmailField(min_length=3)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'password2']
+        extra_kwargs = {'password': {'write_only': True}}
+        ref_name = "UserPostInfo"
+
+
+class UserSwaggerGetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'is_superuser', 'email']
+        extra_kwargs = {'password': {'write_only': True}}
+        ref_name = "UserInfo"
