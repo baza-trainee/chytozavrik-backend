a00965b2feacc40a8c10f2233288df544656243d
Danil Tumanov <dania.tumanov@gmail.com>
2023-08-04 17:20:11 +0300

    add url for avatars

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -8,5 +8,6 @@ router = DefaultRouter()
 router.register(r'users', views.UserViewSet, basename='user')
 urlpatterns = [
     path('users/me/', views.MeAPIView.as_view(), name='me'),
+    path('avatars/', views.ChildAvatarAPIView.as_view(), name='avatars')
 ]
 urlpatterns += router.urls
