5205b2d6f0ec0df488481057f52dcc54f5b693ab
Danil Tumanov <dania.tumanov@gmail.com>
2023-08-04 15:50:24 +0300

    set up media files

--- chytozavrik/settings/base.py
+++ chytozavrik/settings/base.py
@@ -130,7 +130,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = BASE_DIR / 'static',
-STATIC_ROOT = BASE_DIR /'staticfiles_build' /'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
