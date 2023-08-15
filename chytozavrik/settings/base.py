646572e4d3ce553e579f1a6c995582c785773a32
Danil Tumanov <dania.tumanov@gmail.com>
2023-08-15 14:01:28 +0300

    add chytozavrik/settings/base.py

--- chytozavrik/settings/base.py
+++ chytozavrik/settings/base.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'drf_yasg',
     'corsheaders',
+    'django_cleanup.apps.CleanupConfig',
     'user_profile.apps.UserProfileConfig',
     'quiz.apps.QuizConfig'
 ]
