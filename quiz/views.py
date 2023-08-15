f81474f2898bccada71872e46d923525ad718ce1
Danil Tumanov <dania.tumanov@gmail.com>
2023-08-15 13:50:44 +0300

    create BookViewSet

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.viewsets import ModelViewSet
+from .models import Book
+from .serializers import BookSerializers, BookPatchSerializer
 
-# Create your views here.
+
+class BookViewSet(ModelViewSet):
+    serializer_class = BookPatchSerializer
+    queryset = Book.objects.all()
+    parser_classes = (MultiPartParser, FormParser)
+
+    def get_serializer_class(self):
+        if self.action == 'partial_update':  # This corresponds to the PATCH method
+            return BookPatchSerializer
+        return BookSerializers
