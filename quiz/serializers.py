ce688a2990c7ead67e856c1caaf978967e35e9d5
Danil Tumanov <dania.tumanov@gmail.com>
2023-08-16 15:43:33 +0300

    add serializers for RecommendationBook

--- quiz/serializers.py
+++ quiz/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Book
+from .models import Book, RecommendationBook
 
 
 class BookSerializers(serializers.ModelSerializer):
@@ -12,3 +12,17 @@ class BookPatchSerializer(BookSerializers):
     title = serializers.CharField(required=False)
     author = serializers.CharField(required=False)
     cover_image = serializers.ImageField(required=False)
+
+
+class RecommendationBookSerializer(BookSerializers):
+    recommendation_id = serializers.SerializerMethodField()
+
+    def get_recommendation_id(self, obj):
+        recommendation = obj.recommendations.first()
+        return recommendation.id if recommendation else None
+
+
+class RecommendationBookCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RecommendationBook
+        fields = '__all__'
