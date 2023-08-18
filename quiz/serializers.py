57a61edef97db80ba1aaca7ec267dd348af4aab9
Danil Tumanov <dania.tumanov@gmail.com>
2023-08-18 12:36:26 +0300

    create serializers for Quiz, Question and Answer models

--- quiz/serializers.py
+++ quiz/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Book, RecommendationBook
+from django.db import transaction
+from .models import Book, RecommendationBook, Quiz, Question, Answer, TrueAnswer, QuizReward
 
 
 class BookSerializer(serializers.ModelSerializer):
@@ -25,4 +26,68 @@ class RecommendationBookSerializer(BookSerializer):
 class RecommendationBookCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = RecommendationBook
+        fields = '__all__'
+
+
+class QuizSerializer(BookSerializer):
+    quizz_id = serializers.SerializerMethodField()
+
+    def get_quizz_id(self, obj):
+        quiz = obj.quiz
+        return quiz.id if quiz else None
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+    is_true = serializers.BooleanField(default=False, write_only=True)
+
+    class Meta:
+        model = Answer
+        exclude = ('question',)
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    answers = AnswerSerializer(many=True)
+
+    class Meta:
+        model = Question
+        exclude = ('quiz',)
+
+
+class QuizInfoSerializer(serializers.ModelSerializer):
+    questions = QuestionSerializer(many=True)
+
+    class Meta:
+        model = Quiz
+        exclude = ('book', 'created_at', 'updated_at')
+
+
+class QuizCreateSerializer(serializers.ModelSerializer):
+    questions = QuestionSerializer(many=True)
+
+    @transaction.atomic
+    def create(self, validated_data):
+        quiz = Quiz.objects.create(book=validated_data.pop('book'))
+        questions_data = validated_data.pop('questions')
+        self.create_questions(quiz, questions_data)
+        return quiz
+
+    def create_questions(self, quiz, questions):
+        for question in questions:
+            current_question = Question.objects.create(quiz=quiz, text=question.pop('text'))
+            answers_data = question.pop('answers')
+            self.create_answers(current_question, answers_data)
+
+    def create_answers(self, question, answers):
+        true_answer = 0
+        for answer in answers:
+            current_answer = Answer.objects.create(question=question, text=answer['text'])
+            if answer['is_true']:
+                true_answer += 1
+                TrueAnswer.objects.create(question=question, answer=current_answer)
+        if true_answer != 1:
+            raise serializers.ValidationError(
+                {'detail': f'You need to mark exactly one true answer for question {question.text}'})
+
+    class Meta:
+        model = Quiz
         fields = '__all__'
