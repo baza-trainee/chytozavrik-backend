7042cc1c9608b8d60297979aa1bafe2a9916b149
Danil Tumanov <dania.tumanov@gmail.com>
2023-08-20 13:40:30 +0300

    add QuizViewSet

--- quiz/views.py
+++ quiz/views.py
@@ -2,7 +2,7 @@ from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework import mixins
 from rest_framework import permissions
-from .models import Book, RecommendationBook
+from .models import Book, RecommendationBook, Quiz
 from . import serializers
 
 
@@ -39,3 +39,30 @@ class RecommendationBookViewSet(mixins.CreateModelMixin,
             permission_classes = [permissions.IsAdminUser]
         return [permission() for permission in permission_classes]
 
+
+class QuizViewSet(mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.DestroyModelMixin,
+                   mixins.ListModelMixin,
+                   GenericViewSet):
+
+    def get_permissions(self):
+        if self.action == 'create':
+            permission_classes = [permissions.IsAdminUser]
+        else:
+            permission_classes = [permissions.IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
+    def get_queryset(self):
+        if self.action == 'list':
+            return Book.objects.filter(quiz__isnull=False)
+        elif self.action == 'retrieve':
+            Quiz.objects.all().prefetch_related('questions__answers')
+        return Quiz.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return serializers.QuizSerializer
+        elif self.action == 'retrieve':
+            return serializers.QuizInfoSerializer
+        return serializers.QuizCreateSerializer
