43972404a3875c30ec921014a0c09e25ed958a1d
Danil Tumanov <dania.tumanov@gmail.com>
2023-08-21 13:31:49 +0300

    add router for QuizRewardViewSet

--- quiz/urls.py
+++ quiz/urls.py
@@ -8,6 +8,7 @@ router = DefaultRouter()
 router.register(r'books', views.BookViewSet, basename='book')
 router.register(r'recommendation-books', views.RecommendationBookViewSet, basename='recommendation-books')
 router.register(r'quizzes', views.QuizViewSet, basename='quizz')
+router.register(r'quizzes-rewards', views.QuizRewardViewSet, basename='quiz-reward')
 
 urlpatterns = []
 urlpatterns += router.urls
