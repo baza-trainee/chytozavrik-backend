6b7cff7415c9829e0479ce55da9d18bee2d7899a
atelay <deadroll95@gmail.com>
2023-10-30 15:53:41 +0200

    feat: Added pagination and search for partners resource. Added new fields "name" and "created_at". Fixed duplication when adding new partners.

--- partners/models.py
+++ partners/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 
 class Partner(models.Model):
+    name = models.CharField(max_length=255)
     img = models.ImageField(upload_to='partners/')
     link = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = (('name', 'link'),)
--- partners/views.py
+++ partners/views.py
@@ -1,18 +1,35 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny, IsAdminUser
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework import filters, status
+from rest_framework.response import Response
+
 from .models import Partner
 from .serializers import PartnerSerializer
+from .pagination import PartnerPagination
 
 
 class PartnerViewSet(ModelViewSet):
-    queryset = Partner.objects.all()
+    queryset = Partner.objects.order_by("id")
     serializer_class = PartnerSerializer
     parser_classes = (MultiPartParser, FormParser)
+    pagination_class = PartnerPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["name"]
 
     def get_permissions(self):
-        if self.action == 'list' or self.action == 'retrieve':
+        if self.action == "list" or self.action == "retrieve":
             return [AllowAny()]
         return [IsAdminUser()]
 
-
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        existing_partner = Partner.objects.filter(
+            name__iexact=data["name"], link__iexact=data["link"]
+        ).exists()
+        if existing_partner:
+            return Response(
+                {"detail": f"Партнер '{data['name']}' вже існує в базі даних."},
+                status=status.HTTP_409_CONFLICT,
+            )
+        return super().create(request, *args, **kwargs)
