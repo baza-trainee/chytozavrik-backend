a999a0fdcbeb8527f10a3b1dab1a32abb3c405c2
atelay <deadroll95@gmail.com>
2023-10-30 16:18:43 +0200

    fix: add env_example.

--- partners/models.py
+++ partners/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Partner(models.Model):
     name = models.CharField(max_length=255)
-    img = models.ImageField(upload_to='partners/')
+    img = models.ImageField(upload_to="partners/")
     link = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        unique_together = (('name', 'link'),)
+        unique_together = (("name", "link"),)
