ed0f4eb3ebc7b9f5d464cc641ae3cc2b870be04a
smile4alice <smile.to.alice@gmail.com>
2023-10-31 22:14:05 +0200

    chore: else  one check filesystem

--- chytozavrik/settings/base.py
+++ chytozavrik/settings/base.py
@@ -141,7 +141,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = (BASE_DIR / "static",)
 STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
-MEDIA_URL = ".vercel/output/static/media/"
+MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / MEDIA_URL
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
