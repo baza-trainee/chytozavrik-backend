627455d76e0987613913ac4c02e15993ce563e77
atelay <deadroll95@gmail.com>
2023-11-03 14:19:46 +0200

    feat: Add endpoint for quizz stats. add new field for childquizattempt table.

--- stats/urls.py
+++ stats/urls.py
@@ -7,6 +7,17 @@ app_name = "statistics"
 router = DefaultRouter()
 urlpatterns = [
     path("statistics/", include(router.urls)),
-    path("statistics/users/", views.UserStatisticsView.as_view(), name="users-statistics"),
-    path("statistics/child/", views.ChildStatisticsView.as_view(), name="child-statistics"),
+    path(
+        "statistics/users/", views.UserStatisticsView.as_view(), name="users-statistics"
+    ),
+    path(
+        "statistics/child/",
+        views.ChildStatisticsView.as_view(),
+        name="child-statistics",
+    ),
+    path(
+        "statistics/quizzes/",
+        views.QuizStatisticsView.as_view(),
+        name="quizz-statistics",
+    ),
 ]
